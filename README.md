4676b732c18066dfe4f1777a29e9a1d9c33d8e7b
Fernanda Lustosa <lustosafk@gmail.com>
2024-07-08 00:32:11 -0300

    final project

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# ray-tracing-in-one-weekend
+# Ray Tracer in Rust
+
+This is a simple ray tracer project implemented in Rust, capable of generating images by rendering spheres in a scene described using XML files.
+
+## Features
+
+- **Ray Tracing**: Utilizes ray tracing techniques to simulate the interaction of light with objects in a scene.
+- **XML Scene Description**: Scenes are described using XML files, allowing for straightforward sphere placements and configurations.
+- **Materials and Textures**: Supports various materials for realistic object appearances.
+- **Output**: Generates rendered images depicting spheres with shading and lighting effects.
+
+## Getting Started
+
+### Prerequisites
+
+- [Rust](https://www.rust-lang.org/) installed on your system.
+
+### Installation
+
+Clone the repository:
+
+```bash
+git clone https://github.com/caciolucas/ray-tracing-in-one-weekend-rust.git
+cd ray-tracing-in-one-weekend-rust
+```
+
+Build the project:
+
+```bash
+cargo build
+```
+
+### Usage
+
+To generate a scene based on an XML file, execute:
+
+```bash
+cargo run
+```
+
+Then type the path for your XML scene file.
+
+
+### Example XML Scene File
+
+```xml
+<RT>
+    <film filename="path_to_new_img.ppm">
+    <camera look_from="x y z" look_at="x y z" up="x y z" aperture="a"/>
+    <world>
+        <material type="material_type" center="x y z" />
+        <object type="sphere" center="x y z" radius="r" />
+    </world>
+</RT>
+
+```
--- src/main.rs
+++ src/main.rs
@@ -23,64 +23,6 @@ use vec::{Color, Point3, Vec3};
 
 use crate::material::Scatter;
 
-fn random_scene() -> World {
-    let mut rng = rand::thread_rng();
-    let mut world = World::new();
-
-    let ground_mat = Arc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
-    let ground_sphere = Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_mat);
-
-    world.push(Box::new(ground_sphere));
-
-    for a in -11..=11 {
-        for b in -11..=11 {
-            let choose_mat: f64 = rng.gen();
-            let center = Point3::new(
-                (a as f64) + rng.gen_range(0.0..0.9),
-                0.2,
-                (b as f64) + rng.gen_range(0.0..0.9),
-            );
-
-            if choose_mat < 0.8 {
-                // Diffuse
-                let albedo = Color::random(0.0..1.0) * Color::random(0.0..1.0);
-                let sphere_mat = Arc::new(Lambertian::new(albedo));
-                let sphere = Sphere::new(center, 0.2, sphere_mat);
-
-                world.push(Box::new(sphere));
-            } else if choose_mat < 0.95 {
-                // Metal
-                let albedo = Color::random(0.4..1.0);
-                let fuzz = rng.gen_range(0.0..0.5);
-                let sphere_mat = Arc::new(Metal::new(albedo, fuzz));
-                let sphere = Sphere::new(center, 0.2, sphere_mat);
-
-                world.push(Box::new(sphere));
-            } else {
-                // Glass
-                let sphere_mat = Arc::new(Dielectric::new(1.5));
-                let sphere = Sphere::new(center, 0.2, sphere_mat);
-
-                world.push(Box::new(sphere));
-            }
-        }
-    }
-
-    let mat1 = Arc::new(Dielectric::new(1.5));
-    let mat2 = Arc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
-    let mat3 = Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
-
-    let sphere1 = Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat1);
-    let sphere2 = Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat2);
-    let sphere3 = Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat3);
-
-    world.push(Box::new(sphere1));
-    world.push(Box::new(sphere2));
-    world.push(Box::new(sphere3));
-
-    world
-}
-
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
         // If we've exceeded the ray bounce limit, no more light is gathered
@@ -298,28 +240,6 @@ fn main() {
     const SAMPLES_PER_PIXEL: u64 = 500;
     const MAX_DEPTH: u64 = 50;
 
-    /*
-    // World
-    let world = random_scene();
-
-    // Camera
-    let lookfrom = Point3::new(13.0, 2.0, 3.0);
-    let lookat = Point3::new(0.0, 0.0, 0.0);
-    let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = 10.0;
-    let aperture = 0.1;
-
-    let cam = Camera::new(
-        lookfrom,
-        lookat,
-        vup,
-        20.0,
-        ASPECT_RATIO,
-        aperture,
-        dist_to_focus,
-    );
-    */
-
     let new_file = File::create(&img_name)
         .expect("Failed to create file.");
     let mut new_file = BufWriter::new(new_file);
@@ -327,10 +247,6 @@ fn main() {
     writeln!(new_file, "P3").expect("Filed to write");
     writeln!(new_file, "{} {}", IMAGE_WIDTH, IMAGE_HEIGHT).expect("Filed to write");
     writeln!(new_file, "255").expect("Filed to write");
-
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("255");
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining: {}", j + 1);
@@ -357,8 +273,6 @@ fn main() {
 
         for pixel_color in scanline {
             writeln!(new_file, "{}", pixel_color.format_color(SAMPLES_PER_PIXEL)).expect("Filed to write");
-
-            println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
         }
     }
 
